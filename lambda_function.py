c6bd9b604aeb99187246efd78e2b64c4c9d6ca1f
alex vamv <vamvas@outlook.com.gr>
2024-06-03 22:03:38 +0100

    Updating lambda to simpler version

--- lambda_function.py
+++ lambda_function.py
@@ -1,32 +1,7 @@
-# lambda_function.py
 import json
-import boto3
-# import joblib
-from io import BytesIO
-
-s3 = boto3.client('s3')
-
-def download_model():
-    response = s3.get_object(
-        Bucket='trained-ml-models-repository',
-        Key='models/decision_tree_model.pkl')
-    
-    model_str = response['Body'].read()
-    # model = joblib.load(BytesIO(model_str))
-
-    return model_str
-
-
-
 
 def lambda_handler(event, context):
-    # body = json.loads(event['body'])
-    # input_data = body['input']
-    # prediction = model.predict([input_data])
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps({'prediction': prediction.tolist()})
-    # }
-    model = download_model()
-    
-    return model
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
